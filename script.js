be764d705dc0d3c4eecb2a1b3d10d52f89fa0113
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-08-03 17:40:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function loadTask() {
+    const storedTask = JSON.parse(localStorage.getItem('task') || '[]');
+    storedTask.forEach(task => addTask(taskText, false));
+}
+function addTask(taskText, saveToLocalStorage = true) {
+    const taskList = document.getElementById('taskList');
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskText;
+    taskList.appendChild(taskItem);
+
+    if (saveToLocalStorage) {
+        const storedTask = JSON.parse(localStorage.getItem('task') || '[]');
+        storedTask.push(taskText);
+        localStorage.setItem('task', JSON.stringify(storedTask));
+    }
+}
+document.addEventListener('DOMContentLoaded', () => { loadTask(); });
